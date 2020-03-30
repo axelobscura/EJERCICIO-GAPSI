1e8682ce51a9fb88116d1dd03ae3b670ce91a144
Axel Obscura <axosar@gmail.com>
2020-03-30 13:52:25 -0600

    Header-Navbar section

---
+++ gapsi/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">e-Commerce Gapsi</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </nav>
+        </>
+    )
+}
